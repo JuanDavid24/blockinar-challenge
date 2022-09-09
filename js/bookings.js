9684efe3d0b02714ae66f01a3c6e8eb6f3820e92
JuanDavid24 <david.juan90.jd@gmail.com>
2022-09-09 15:25:47 -0300

    +get all bookings + sort by last name
    bookings.html:
    
    -tabla para mostrar lista de bookings+
     -ordene codigo
    ------------------------------------
    bookings.js
    
    -funcion para agregar booking a una fila.
    -funcion para ordenar por apellido
    -las bookings se guardan en session storage
    -se limpia la tabla antes de agregarle filas

---
+++ js/bookings.js
@@ -0,0 +1,59 @@
+jQuery( () => {
+    $("#get-bookings-btn").on("click", async () => {
+        $("#tbody").empty() //limpiar tabla
+        get_all_bookings();  
+    });
+
+    $("#sort-by-name-btn").on("click", () => {
+        $("#tbody").empty() //limpiar tabla
+        bookings = JSON.parse(sessionStorage.bookings);
+        sort_bookings_by_name(bookings);
+    });
+});
+
+// Realiza un GET a un endpoint y devuelve la data del response
+const get_from_API = async ( URL, params='' ) => {
+    const response = await axios.get( URL + params );
+    return response.data
+}
+
+// --- Obtiene todas las reservas en formato JSON y las muestra en una tabla ---
+async function get_all_bookings() {
+    get_from_API('https://api-challenge.blockinar.io/bookings')
+        .then(bookings => {
+            sessionStorage.setItem('bookings', JSON.stringify(bookings));
+            bookings.forEach(booking => booking_to_table_row(booking));
+        })
+        .catch(
+            err => alert(`No se pudo completar la solicitud. Error: ${err}`)
+        );
+}
+
+const booking_to_table_row = booking => {
+    $('#tbody').append(`<tr>
+        <th scope="row">${booking.id}</th>
+        <td>${booking.first_name}</td>
+        <td>${booking.last_name}</td>
+        <td>${booking.check_in_date}</td>
+        <td>${booking.check_out_date}</td>
+        <td>${booking.booking_status}</td>
+        <td>${booking.number_of_guests}</td>
+        <td>${booking.price_per_night}</td>
+        <td>${
+            (booking.room_id===null ? '' : booking.room_id)
+        }</td>
+        </tr>`);    
+    };
+
+const sort_bookings_by_name = bookings => {
+    bookings.sort( function(a, b){
+        let x = a.last_name.toLowerCase();
+        let y = b.last_name.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+      });
+      bookings.forEach(booking => booking_to_table_row(booking));
+};
+// --- get booking por fecha de check in ---
+// 'https://api-challenge.blockinar.io/bookings?check_in=2021-10-2415:00:00'
