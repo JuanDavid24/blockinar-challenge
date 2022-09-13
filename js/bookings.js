e175a5ebbaf9ad60ee0830b26fd185b351ac8ad9
JuanDavid24 <david.juan90.jd@gmail.com>
2022-09-13 12:43:03 -0300

    cambio a datatables

--- js/bookings.js
+++ js/bookings.js
@@ -1,13 +1,54 @@
+const DATETIME_FROM_PATTERN = "YYYY-MM-DD'T'HH:mm:ss";
+const DATETIME_TO_PATTERN   = "DD/MM/YYYY HH:mm:ss";
 jQuery( () => {
+
     $("#pill-list-bookings-tab").on("click", () => {
-        $("#tbody").empty(); //limpiar tabla
-        get_all_bookings();
-        $("#refresh-btn").show();
+        let table_is_initialized = $.fn.DataTable.isDataTable( '#booking-table' )
+          
+        if (!table_is_initialized) {
+            let booking_table = $('#booking-table').DataTable({
+                processing: true,
+                ajax: {
+                    "url": "https://api-challenge.blockinar.io/bookings",
+                    dataSrc:"",
+                },
+                columns: [
+                    { data: 'id'},
+                    { data: 'first_name'},
+                    { data: 'last_name' },
+                    { 
+                        data: 'check_in_date',
+                        render: $.fn.dataTable.render.moment(DATETIME_FROM_PATTERN, DATETIME_TO_PATTERN)
+                    },
+                    { 
+                        data: 'check_out_date',
+                        // render: $.fn.dataTable.render.moment(DATETIME_FROM_PATTERN, DATETIME_TO_PATTERN), 
+                    },
+                    { data: 'booking_status' },
+                    { data: 'number_of_guests' },
+                    { data: 'price_per_night' },
+                    { data: 'room_id' },
+                ],
+
+            });
+        }
+        // --- Actualizo tabla ---
+        else {$('#booking-table').DataTable().ajax.reload()}
+        
     });
-    
+
+    $("#list-by-date-form").on("submit", function(){
+        let inputDate = $("#list-by-date-form .form-control");
+        if (inputDate.val()) {
+            console.log(inputDate.val())
+        }
+      });
+
     $("#refresh-btn").on("click", async () => {
-        $("#tbody").empty(); //limpiar tabla
-        get_all_bookings();
+        // $("#tbody").empty(); //limpiar tabla
+        // get_all_bookings();
+
+        $('#booking-table').DataTable().ajax.reload();
     });
 
     $("#sort-by-name-btn").on("click", () => {
@@ -28,13 +69,24 @@ async function get_all_bookings() {
     await get_from_API('https://api-challenge.blockinar.io/bookings')
         .then(bookings => {
             sessionStorage.setItem('bookings', JSON.stringify(bookings));
-            bookings.forEach(booking => booking_to_table_row(booking));
+            // bookings.forEach(booking => booking_to_table_row(booking));
+            data_set = make_dataset(bookings)
+            console.log(data_set)
+            console.log(bookings[0])
+
         })
         .catch(
             err => alert(`No se pudo completar la solicitud. Error: ${err}`)
         );
 }
 
+const make_dataset = object_array => {
+    let data_set = []
+    object_array.forEach(obj => {
+        data_set.push( Object.values(obj) )
+    });
+    return data_set
+};
 const booking_to_table_row = booking => {
     $('#tbody').append(`<tr>
         <th scope="row">${booking.id}</th>
@@ -48,7 +100,7 @@ const booking_to_table_row = booking => {
         <td>${
             (booking.room_id===null ? '' : booking.room_id)
         }</td>
-        </tr>`);    
+        </tr>`);
     };
 
 const sort_bookings_by_name = bookings => {
