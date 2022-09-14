574eb6157ef42d5aec9c942c4f24713b59600d4d
JuanDavid24 <david.juan90.jd@gmail.com>
2022-09-14 04:50:18 -0300

    +buscar y editar: +search form +edit form
    -Se busca la booking por ID
    -Se completan los datos de edit form con los de la booking devuelta por la API.
    -Solo los necesarios son editables

--- js/bookings.js
+++ js/bookings.js
@@ -1,15 +1,10 @@
-const DATETIME_FROM_PATTERN = "YYYY-MM-DD'T'HH:mm:ss";
-const DATETIME_TO_PATTERN   = "DD/MM/YYYY HH:mm:ss";
-jQuery( () => {
-    
+jQuery( () => { 
     $("#pill-list-bookings-tab").on("click", () => {
         let table_is_initialized = $.fn.DataTable.isDataTable( '#booking-table' )
           
         if (!table_is_initialized) {
             let booking_table = $('#booking-table').DataTable({
                 processing: true,
-                
-
                 ajax: {
                     "url": "https://api-challenge.blockinar.io/bookings",
                     dataSrc:"",
@@ -33,31 +28,47 @@ jQuery( () => {
         else $('#booking-table').DataTable().ajax.reload()
     });
 
-    $("#list-by-date-form").on("submit", function(){
-        let inputDate = $("#list-by-date-form .form-control");
-        if (inputDate.val()) {
-            console.log(inputDate.val())
-        }
+    $("#search-booking-form").on("submit", () => { 
+        let input = $("#search-booking-form input");
+        if ( input.val() ) {
+            get_from_API( 'https://api-challenge.blockinar.io/bookings/' + input.val() )
+            .then(result => {
+                $("#id-to-edit").val(result.id);
+                $("#check-in-to-edit").val(result.check_in_date);
+                $("#check-out-to-edit").val(result.check_out_date);
+                $("#guests-to-edit").val(result.number_of_guests);
+                $("#room-id-to-edit").val(result.room_id);
+                $("#price-to-edit").val(result.price_per_night);
+                $("#first-name-to-edit").val(result.first_name);
+                $("#last-name-to-edit").val(result.last_name);
+                $("#status-to-edit").val(result.booking_status);
+                console.log(result)
+            })
+            .catch(
+                err => alert(`No se pudo completar la solicitud. Error: ${err}`)
+            );}
       });
 
     $("#refresh-btn").on("click", async () => {
-        // $("#tbody").empty(); //limpiar tabla
-        // get_all_bookings();
-        const myDate = moment('2021-10-27T12:00:00', DATETIME_FROM_PATTERN).toDate();
-        console.log(myDate)
-        // $('#booking-table').DataTable().ajax.reload();
+        let table_is_initialized = $.fn.DataTable.isDataTable( '#booking-table' )
+        if (table_is_initialized) {
+        $('#booking-table').DataTable().ajax.reload(); }
     });
 
-    $("#sort-by-name-btn").on("click", () => {
-        $("#tbody").empty() //limpiar tabla
-        bookings = JSON.parse(sessionStorage.bookings);
-        sort_bookings_by_name(bookings);
-    });
+    // $("#sort-by-name-btn").on("click", () => {
+    //     $("#tbody").empty() //limpiar tabla
+    //     bookings = JSON.parse(sessionStorage.bookings);
+    //     sort_bookings_by_name(bookings);
+    // });
 });
 
 // Realiza un GET a un endpoint y devuelve la data del response
 const get_from_API = async ( URL, params='' ) => {
     const response = await axios.get( URL + params );
+    return response.data
+}
+const post_to_API = async ( URL, params='' ) => {
+    const response = await axios.post( URL + params );
     return response.data
 }
 
@@ -77,38 +88,38 @@ async function get_all_bookings() {
         );
 }
 
-const make_dataset = object_array => {
-    let data_set = []
-    object_array.forEach(obj => {
-        data_set.push( Object.values(obj) )
-    });
-    return data_set
-};
-const booking_to_table_row = booking => {
-    $('#tbody').append(`<tr>
-        <th scope="row">${booking.id}</th>
-        <td>${booking.first_name}</td>
-        <td>${booking.last_name}</td>
-        <td>${booking.check_in_date}</td>
-        <td>${booking.check_out_date}</td>
-        <td>${booking.booking_status}</td>
-        <td>${booking.number_of_guests}</td>
-        <td>${booking.price_per_night}</td>
-        <td>${
-            (booking.room_id===null ? '' : booking.room_id)
-        }</td>
-        </tr>`);
-    };
+// const make_dataset = object_array => {
+//     let data_set = []
+//     object_array.forEach(obj => {
+//         data_set.push( Object.values(obj) )
+//     });
+//     return data_set
+// };
+// const booking_to_table_row = booking => {
+//     $('#tbody').append(`<tr>
+//         <th scope="row">${booking.id}</th>
+//         <td>${booking.first_name}</td>
+//         <td>${booking.last_name}</td>
+//         <td>${booking.check_in_date}</td>
+//         <td>${booking.check_out_date}</td>
+//         <td>${booking.booking_status}</td>
+//         <td>${booking.number_of_guests}</td>
+//         <td>${booking.price_per_night}</td>
+//         <td>${
+//             (booking.room_id===null ? '' : booking.room_id)
+//         }</td>
+//         </tr>`);
+//     };
 
-const sort_bookings_by_name = bookings => {
-    bookings.sort( function(a, b){
-        let x = a.last_name.toLowerCase();
-        let y = b.last_name.toLowerCase();
-        if (x < y) {return -1;}
-        if (x > y) {return 1;}
-        return 0;
-      });
-      bookings.forEach(booking => booking_to_table_row(booking));
-};
+// const sort_bookings_by_name = bookings => {
+//     bookings.sort( function(a, b){
+//         let x = a.last_name.toLowerCase();
+//         let y = b.last_name.toLowerCase();
+//         if (x < y) {return -1;}
+//         if (x > y) {return 1;}
+//         return 0;
+//       });
+//       bookings.forEach(booking => booking_to_table_row(booking));
+// };
 // --- get booking por fecha de check in ---
 // 'https://api-challenge.blockinar.io/bookings?check_in=2021-10-2415:00:00'
